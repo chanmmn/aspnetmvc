6ea469e12de58d3a6cba1b10bc5cfbba24da7a34
chanmmn <33271651+chanmmn@users.noreply.github.com>
2021-02-14 17:59:53 +0800

    commit ASP.NET Angular Route
    commit ASP.NET Angular Route

---
+++ WebAppMvcRouteJs/WebAppMvc/Scripts/myscript/route.js
@@ -0,0 +1,16 @@
+﻿var app = angular.module("myApp", ["ngRoute"]);
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "/Scripts/myscript/main.htm"
+        })
+        .when("/red", {
+            templateUrl: "/Scripts/myscript/red.htm"
+        //})
+        //.when("/green", {
+        //    templateUrl: "green.htm"
+        //})
+        //.when("/blue", {
+        //    templateUrl: "blue.htm"
+        });
+});
